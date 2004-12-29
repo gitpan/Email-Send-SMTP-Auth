464e411a1509d7fec57046c59752e68051693a7b
Martin 'Kingpin' Thurn <kingpin@dcswcc.org>
2004-12-29 02:44:24 +0000

    Import of MTHURN/Email-Send-SMTP-Auth-1.007 from CPAN.
    gitpan-cpan-distribution: Email-Send-SMTP-Auth
    gitpan-cpan-version:      1.007
    gitpan-cpan-path:         MTHURN/Email-Send-SMTP-Auth-1.007.tar.gz
    gitpan-cpan-author:       MTHURN
    gitpan-cpan-maturity:     released

--- Auth.pm
+++ Auth.pm
@@ -1,5 +1,5 @@
 package Email::Send::SMTP::Auth;
-# $Id: Auth.pm,v 1.5 2004/07/08 03:23:38 Daddy Exp $
+# $Id: Auth.pm,v 1.7 2004/12/29 02:41:52 Daddy Exp $
 
 use Carp;
 use Net::SMTP_auth;
@@ -8,12 +8,13 @@ use Email::Address;
 use strict;
 
 use vars qw( $VERSION $SMTP $VERBOSE );
-$VERSION = do { my @r = (q$Revision: 1.5 $ =~ /\d+/g); sprintf "%d."."%03d" x $#r, @r };
+$VERSION = do { my @r = (q$Revision: 1.7 $ =~ /\d+/g); sprintf "%d."."%03d" x $#r, @r };
 
 BEGIN
   {
   $VERBOSE = 0;  # Unless our caller sets it otherwise
   } # end of BEGIN block
+use constant DEBUG => 0;
 
 sub send
   {
@@ -57,24 +58,29 @@ sub send
     warn " XXX 'From' header does not parse into an Email::Address\n" if $VERBOSE;
     return 0;
     } # unless
-  # Make sure the to-address is good:
-  my $sTo = $oMessage->header('To');
-  unless ($sTo)
+  # Make sure the to-addresses are good:
+  my @asToAddr;
+ TO_ADDR_HEADER:
+  foreach my $sWhere (qw( To Cc Bcc ))
     {
-    warn " XXX message has no 'To' header\n" if $VERBOSE;
-    return 0;
-    } # unless
-  unless ($sTo ne '')
-    {
-    warn " XXX message has empty 'To' header\n" if $VERBOSE;
-    return 0;
-    } # unless
-  my $oTo = (Email::Address->parse($sTo))[0];
-  unless (ref $oTo)
-    {
-    warn " XXX 'To' header does not parse into an Email::Address\n" if $VERBOSE;
-    return 0;
-    } # unless
+    my $sHeader = $oMessage->header($sWhere);
+    DEBUG && warn " DDD got header=$sHeader= from header=$sWhere=\n";
+    next TO_ADDR_HEADER unless (defined $sHeader);
+    next TO_ADDR_HEADER unless ($sHeader ne '');
+ TO_ADDR:
+    foreach my $oAddr (Email::Address->parse($sHeader))
+      {
+      DEBUG && warn " DDD got o=$oAddr= from header=$sWhere=\n";
+      unless (ref $oAddr)
+        {
+        warn " XXX '$sWhere' portion '$sHeader' does not parse into an Email::Address\n" if $VERBOSE;
+        next TO_ADDR;
+        } # unless
+      my $sAddr = $oAddr->address;
+      DEBUG && warn " DDD got addr=$sAddr= from header=$sWhere=\n";
+      push @asToAddr, $sAddr;
+      } # foreach TO_ADDR
+    } # foreach TO_ADDR_HEADER
   # Now talk to the server:
   $SMTP->mail($oFrom->address);
   unless ($SMTP->ok)
@@ -82,12 +88,16 @@ sub send
     warn sprintf(" XXX SMTP server refused 'from', with error %d: %s", $SMTP->code, $SMTP->message) if $VERBOSE;
     return 0;
     } # unless
-  $SMTP->to($oTo->address);
-  unless ($SMTP->ok)
+  foreach my $sAddr (@asToAddr)
     {
-    warn sprintf(" XXX SMTP server refused 'to', with error %d: %s", $SMTP->code, $SMTP->message) if $VERBOSE;
-    return 0;
-    } # unless
+    $SMTP->to($sAddr);
+    unless ($SMTP->ok)
+      {
+      warn sprintf(" XXX SMTP server refused recipient '%s', with error %d: %s",
+                   $sAddr, $SMTP->code, $SMTP->message) if $VERBOSE;
+      return 0;
+      } # unless
+    } # foreach
   $SMTP->data($oMessage->as_string);
   unless ($SMTP->ok)
     {
--- t/auth.t
+++ t/auth.t
@@ -1,6 +1,8 @@
 use ExtUtils::testlib;
 use Test::More no_plan;
 
+use strict;
+
 BEGIN
   {
   use_ok('Data::Dumper');
@@ -14,6 +16,9 @@ $Email::Send::SMTP::Auth::VERBOSE = 1;
 my $sSMTPserver = $ENV{SMTPSERVER} || 'your SMTP server';
 my $sFrom = $ENV{SMTPTESTFROM} || '';
 my $sTo = $ENV{SMTPTESTTO} || '';
+my $sCc = $ENV{SMTPTESTCC} || '';
+my $sBcc = $ENV{SMTPTESTBCC} || '';
+my $sDTG = scalar(localtime);
 SKIP:
   {
   my $iSkip = 0;
@@ -32,6 +37,14 @@ SKIP:
     diag(qq{The SMTPTESTTO environment variable is not set.  Its value should be an address to which a test email can be sent.});
     $iSkip++;
     } # if
+  if ($sCc eq '')
+    {
+    diag(qq{The SMTPTESTCC environment variable is not set.  Its value should be an address to which a test email can be sent.});
+    } # if
+  if ($sBcc eq '')
+    {
+    diag(qq{The SMTPTESTBCC environment variable is not set.  Its value should be an address to which a test email can be sent.});
+    } # if
   my $sUsername = $ENV{SMTPUSERNAME} || 'that username';
   diag(qq{The SMTPUSERNAME environment variable is not set.  Its value should be a valid login name, if $sSMTPserver requires you to log in.}) unless ($sUsername ne 'that username');
   my $sPassword = $ENV{SMTPPASSWORD} || '';
@@ -43,15 +56,23 @@ SKIP:
   ok $oMessage->body_set(qq{This is a test email.
 It was sent from $sFrom
 It was sent to   $sTo
+It was sent Cc   $sCc
+It was sent Bcc  $sBcc
 It was sent via  $sSMTPserver
+It was sent at   $sDTG
 });
   ok $oMessage->header_set('Subject', 'test message sent by Email-Send-SMTP-Auth t/auth.t');
 
   skip "No test email can be sent, because of missing environment variables.", 3 if $iSkip;
   diag(qq{Test email will be sent From: =$sFrom=});
   diag(qq{Test email will be sent   To: =$sTo=});
+  diag(qq{Test email will be sent   Cc: =$sCc=});
+  diag(qq{Test email will be sent  Bcc: =$sBcc=});
   diag(qq{Test email will be sent  via: =$sSMTPserver=});
+  diag(qq{Test email was     sent   at: =$sDTG=});
   ok $oMessage->header_set('From', $sFrom);
   ok $oMessage->header_set('To', $sTo);
-  ok send SMTP::Auth => $oMessage, $sSMTPserver, $sUsername, $sPassword;
+  ok $oMessage->header_set('Cc', $sCc) if ($sCc ne '');
+  ok $oMessage->header_set('Bcc', $sBcc) if ($sBcc ne '');
+  ok send 'SMTP::Auth' => $oMessage, $sSMTPserver, $sUsername, $sPassword;
   } # end of SKIP block
